6cfc9748819e525f64036f51207f613dbfa3c516
Aria-MUC <55189717+Aria-MUC@users.noreply.github.com>
2020-03-08 12:33:32 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  s<-matrix()
+  set<-function(y){
+    x<<-y 
+    s<<-matrix()
+  }
+  
+  get<-function() x
+  setM<-function() s<<-cacheSolve(x)
+  getM<-function() s
+  list(set=set,get=get,setM=setM,getM=getM)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getM()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setM(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
